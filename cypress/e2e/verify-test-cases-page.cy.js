d658ca0481b74d32e121bce320a7eaebba394034
Kajus <kajus303@gmail.com>
2024-10-02 15:26:50 +0300

    verify test cases page test added

---
+++ cypress/e2e/verify-test-cases-page.cy.js
@@ -0,0 +1,10 @@
+describe("Test Case 7: Verify Test Cases Page", () => {
+  it("Should navigate to Test Cases page successfully", () => {
+    cy.visit("https://automationexercise.com");
+    cy.url().should("eq", "https://automationexercise.com/");
+    cy.get("body").should("be.visible");
+    cy.contains("a", "Test Cases").click();
+    cy.url().should("include", "/test_cases");
+    cy.get("h2.title.text-center").should("have.text", "Test Cases");
+  });
+});
