0578926194c81e5f4f3789329b2aaa8535648637
Kajus <kajus303@gmail.com>
2024-10-03 13:25:42 +0300

    'verify-test-cases-page' update

--- cypress/e2e/verify-test-cases-page.cy.js
+++ cypress/e2e/verify-test-cases-page.cy.js
@@ -1,9 +1,15 @@
 describe("Test Case 7: Verify Test Cases Page", () => {
-  it("Should navigate to Test Cases page successfully", () => {
-    cy.visit("");
-    cy.url().should("eq", "https://automationexercise.com/");
-    cy.get("body").should("be.visible");
+  it("should navigate to Test Cases page successfully", () => {
+    // Navigate to URL "http://automationexercise.com"
+    cy.visit("http://automationexercise.com");
+
+    // Verify that home page is visible successfully
+    cy.get('img[src="/static/images/home/logo.png"]').should("be.visible");
+
+    // Click on 'Test Cases' button
     cy.contains("a", "Test Cases").click();
+
+    // Verify user is navigated to test cases page successfully
     cy.url().should("include", "/test_cases");
     cy.get("h2.title.text-center").should("have.text", "Test Cases");
   });
